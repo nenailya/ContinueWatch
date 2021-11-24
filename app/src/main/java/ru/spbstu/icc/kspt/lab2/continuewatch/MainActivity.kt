b6639e681b1eaf2fb0f2a7693a3c7710c96e121d
n_zgr <nzgr1@mail.ru>
2021-11-24 18:34:06 +0300

    without changes

--- app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MainActivity.kt
+++ app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MainActivity.kt
@@ -1,67 +1,72 @@
 package ru.spbstu.icc.kspt.lab2.continuewatch
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import ru.spbstu.icc.kspt.lab2.continuewatch.databinding.ActivityMainBinding
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
-    val LOG_TAG = "myLogs"
+    val executor: ExecutorService = Executors.newSingleThreadExecutor()
+    private val LOG_TAG = "myLogs"
     private val SECONDS_ELAPSED = "Seconds elapsed"
     var secondsElapsed: Int = 0
     lateinit var textSecondsElapsed: TextView
     private var onTheScreen = true
-
-    var backgroundThread = Thread {
-        try {
-            while (true) {
-                Thread.sleep(1000)
-                if (onTheScreen) {
-                    textSecondsElapsed.post {
-                        textSecondsElapsed.setText("Seconds elapsed: " + secondsElapsed++)
-                    }
-                }
-            }
-        } catch (ex: InterruptedException) {
-        }
-    }
+    lateinit var sharedPref: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        textSecondsElapsed = findViewById(R.id.textSecondsElapsed)
-        textSecondsElapsed.setText("Seconds elapsed: " + secondsElapsed)
-        backgroundThread.start()
+        textSecondsElapsed.text = "Seconds elapsed: $secondsElapsed"
+        sharedPref = getSharedPreferences(SECONDS_ELAPSED, Context.MODE_PRIVATE)
+
+        lifecycleScope.launchWhenResumed {
+            while (true){
+                delay(1000)
+                Log.d(LOG_TAG, "coroutine")
+                textSecondsElapsed.post {
+                    textSecondsElapsed.text = "${secondsElapsed++}"
+            }
+            }
+        }
+
         Log.d(LOG_TAG, "onCreate")
     }
 
     override fun onStop() {
+        executor.shutdown()
         super.onStop()
-        onTheScreen = false
         Log.d(LOG_TAG, "onStop")
     }
 
     override fun onStart() {
-        super.onStart()
         onTheScreen = true
+        secondsElapsed = sharedPref.getInt(SECONDS_ELAPSED, 0)
         Log.d(LOG_TAG, "onStart")
+        super.onStart()
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        outState.run {
-            putInt(SECONDS_ELAPSED,secondsElapsed)
-        }
-        Log.d(LOG_TAG, "onSaveInstanceState")
-        super.onSaveInstanceState(outState)
+    override fun onPause() {
+        super.onPause()
+        onTheScreen = false
+        Log.d(LOG_TAG, "onPause")
     }
 
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        savedInstanceState.run {
-            secondsElapsed = getInt(SECONDS_ELAPSED)
-            textSecondsElapsed.setText("Seconds elapsed: " + secondsElapsed)
-        }
-        Log.d(LOG_TAG, "onRestoreInstanceState")
+    override fun onResume() {
+        super.onResume()
+        onTheScreen = true
+        Log.d(LOG_TAG, "onResume")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(LOG_TAG, "onDestroy")
     }
 }
-
