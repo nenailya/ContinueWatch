5024e6eb71a38acf8706b75f612f14fcf331fe05
n_zgr <nzgr1@mail.ru>
2021-11-24 18:37:54 +0300

    executor

--- app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MainActivity.kt
+++ app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MainActivity.kt
@@ -1,13 +1,10 @@
 package ru.spbstu.icc.kspt.lab2.continuewatch
 
-import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.delay
 import ru.spbstu.icc.kspt.lab2.continuewatch.databinding.ActivityMainBinding
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
@@ -23,21 +20,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         textSecondsElapsed.text = "Seconds elapsed: $secondsElapsed"
-        sharedPref = getSharedPreferences(SECONDS_ELAPSED, Context.MODE_PRIVATE)
-
-        lifecycleScope.launchWhenResumed {
-            while (true){
-                delay(1000)
-                Log.d(LOG_TAG, "coroutine")
-                textSecondsElapsed.post {
-                    textSecondsElapsed.text = "${secondsElapsed++}"
-            }
-            }
-        }
-
         Log.d(LOG_TAG, "onCreate")
+        setContentView(binding.root)
     }
 
     override fun onStop() {
@@ -47,13 +33,23 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onStart() {
+        executor.submit {
+            while (!executor.isShutdown) {
+                Log.d(LOG_TAG, "${Thread.currentThread()} is iterating")
+                Thread.sleep(1000)
+                textSecondsElapsed.post {
+                    textSecondsElapsed.text = "${secondsElapsed++}"
+                }
+            }
+        }
+        super.onStart()
         onTheScreen = true
         secondsElapsed = sharedPref.getInt(SECONDS_ELAPSED, 0)
         Log.d(LOG_TAG, "onStart")
-        super.onStart()
     }
 
     override fun onPause() {
+        executor.shutdown()
         super.onPause()
         onTheScreen = false
         Log.d(LOG_TAG, "onPause")
