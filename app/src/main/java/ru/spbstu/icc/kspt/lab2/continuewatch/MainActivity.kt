a5e9c93eebd03b7a9f6032695667abb0d2d4fa49
n_zgr <nzgr1@mail.ru>
2021-11-26 02:31:38 +0300

    executor

--- app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MainActivity.kt
+++ app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MainActivity.kt
@@ -1,6 +1,5 @@
 package ru.spbstu.icc.kspt.lab2.continuewatch
 
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -9,60 +8,74 @@ import ru.spbstu.icc.kspt.lab2.continuewatch.databinding.ActivityMainBinding
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
+private lateinit var executor: ExecutorService
+
 class MainActivity : AppCompatActivity() {
     private val LOG_TAG = "myLogs"
     private val SECONDS_ELAPSED = "Seconds elapsed"
-    var secondsElapsed: Int = 0
     lateinit var textSecondsElapsed: TextView
+    var secondsElapsed: Int = 0
     private var onTheScreen = true
-    lateinit var sharedPref: SharedPreferences
-    private val executor = (application as MyApplication).executor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
-        textSecondsElapsed.text = "Seconds elapsed: $secondsElapsed"
-        Log.d(LOG_TAG, "onCreate")
+        textSecondsElapsed = binding.textSecondsElapsed
         setContentView(binding.root)
+        Log.d(LOG_TAG, "onCreate")
+    }
+
+    override fun onStart() {
+        executor = Executors.newSingleThreadExecutor()
+        executor.execute {
+            while (!executor.isShutdown) {
+                if (onTheScreen) {
+                    Thread.sleep(1000)
+                    Log.d(LOG_TAG, "${Thread.currentThread()}")
+                    textSecondsElapsed.post {
+                        textSecondsElapsed.text = "Seconds elapsed: " + secondsElapsed++
+                    }
+                }
+            }
+        }
+        Log.d(LOG_TAG, "onStart")
+        super.onStart()
+    }
+
+    override fun onPause() {
+        executor.shutdown()
+        onTheScreen = false
+        super.onPause()
+        Log.d(LOG_TAG, "onPause")
     }
 
     override fun onStop() {
         executor.shutdown()
         super.onStop()
+        onTheScreen = false
+
         Log.d(LOG_TAG, "onStop")
     }
-
-    override fun onStart() {
-        executor.submit {
-            while (!executor.isShutdown) {
-                Log.d(LOG_TAG, "${Thread.currentThread()} is iterating")
-                Thread.sleep(1000)
-                textSecondsElapsed.post {
-                    textSecondsElapsed.text = "${secondsElapsed++}"
-                }
-            }
-        }
-        super.onStart()
-        onTheScreen = true
-        secondsElapsed = sharedPref.getInt(SECONDS_ELAPSED, 0)
-        Log.d(LOG_TAG, "onStart")
-    }
-
-    override fun onPause() {
-        executor.shutdown()
-        super.onPause()
-        onTheScreen = false
-        Log.d(LOG_TAG, "onPause")
-    }
-
     override fun onResume() {
         super.onResume()
         onTheScreen = true
         Log.d(LOG_TAG, "onResume")
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(LOG_TAG, "onDestroy")
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.run {
+            putInt(SECONDS_ELAPSED, secondsElapsed)
+        }
+        Log.d(LOG_TAG, "onSaveInstanceState")
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        savedInstanceState.run {
+            secondsElapsed = getInt(SECONDS_ELAPSED)
+            textSecondsElapsed.text = "Seconds elapsed: " + secondsElapsed
+        }
+        Log.d(LOG_TAG, "onRestoreInstanceState")
     }
 }
