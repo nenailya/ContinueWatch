f31ca0f6518d49a0708962c026470b4c3f37787b
n_zgr <nzgr1@mail.ru>
2021-11-27 23:44:48 +0300

    coroutine

--- app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MainActivity.kt
+++ app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MainActivity.kt
@@ -1,15 +1,11 @@
 package ru.spbstu.icc.kspt.lab2.continuewatch
 
-import android.annotation.SuppressLint
-import android.content.Context
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.isActive
 
 class MainActivity : AppCompatActivity() {
     val LOG_TAG = "myLogs"
@@ -17,13 +13,11 @@ class MainActivity : AppCompatActivity() {
     var secondsElapsed: Int = 0
     lateinit var textSecondsElapsed: TextView
     private var onTheScreen = true
-    lateinit var sharedPref: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         textSecondsElapsed = findViewById(R.id.textSecondsElapsed)
-        sharedPref = getSharedPreferences(SECONDS_ELAPSED, Context.MODE_PRIVATE)
         lifecycleScope.launchWhenResumed {
             while (onTheScreen) {
                 delay(1000)
@@ -37,18 +31,30 @@ class MainActivity : AppCompatActivity() {
     override fun onStop() {
         super.onStop()
         onTheScreen = false
-        with(sharedPref.edit()) {
-            putInt(SECONDS_ELAPSED, secondsElapsed)
-            apply()
-        }
         Log.d(LOG_TAG, "onStop")
     }
 
     override fun onStart() {
         super.onStart()
         onTheScreen = true
-        secondsElapsed = sharedPref.getInt(SECONDS_ELAPSED, 0)
         Log.d(LOG_TAG, "onStart")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.run {
+            putInt(SECONDS_ELAPSED, secondsElapsed)
+        }
+        Log.d(LOG_TAG, "onSaveInstanceState")
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        savedInstanceState.run {
+            secondsElapsed = getInt(SECONDS_ELAPSED)
+            textSecondsElapsed.text = "Seconds elapsed: $secondsElapsed"
+        }
+        Log.d(LOG_TAG, "onRestoreInstanceState")
     }
 }
 
