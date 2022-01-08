89c8636b179aa6da2442c489857efda567e0e6a1
n_zgr <nzgr1@mail.ru>
2022-01-08 21:21:20 +0300

    fixes for the lab5 coroutine

--- app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MainActivity.kt
+++ app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MainActivity.kt
@@ -6,54 +6,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 
 class MainActivity : AppCompatActivity() {
     val LOG_TAG = "myLogs"
-    private val SECONDS_ELAPSED = "Seconds elapsed: "
+    private val SECONDS_ELAPSED = "Seconds elapsed"
     var secondsElapsed: Int = 0
     lateinit var textSecondsElapsed: TextView
-    private var onTheScreen = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         textSecondsElapsed = findViewById(R.id.textSecondsElapsed)
         lifecycleScope.launchWhenResumed {
-            while (onTheScreen) {
+            while (isActive) {
                 delay(1000)
                 Log.d(LOG_TAG, SECONDS_ELAPSED + secondsElapsed)
-                textSecondsElapsed.text = SECONDS_ELAPSED + secondsElapsed++
+                textSecondsElapsed.text = "${secondsElapsed++}"
             }
         }
         Log.d(LOG_TAG, "onCreate")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        onTheScreen = false
-        Log.d(LOG_TAG, "onStop")
-    }
-    override fun onPause() {
-        onTheScreen = false
-        super.onPause()
-        Log.d(LOG_TAG, "onPause")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        onTheScreen = true
-        Log.d(LOG_TAG, "onResume")
-    }
-
-    override fun onStart() {
-        super.onStart()
-        onTheScreen = true
-        Log.d(LOG_TAG, "onStart")
-    }
-
-    override fun onDestroy() {
-        Log.d(LOG_TAG, "onDestroy")
-        super.onDestroy()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -68,7 +40,7 @@ class MainActivity : AppCompatActivity() {
         super.onRestoreInstanceState(savedInstanceState)
         savedInstanceState.run {
             secondsElapsed = getInt(SECONDS_ELAPSED)
-            textSecondsElapsed.text = "Seconds elapsed: $secondsElapsed"
+            textSecondsElapsed.text = "$secondsElapsed"
         }
         Log.d(LOG_TAG, "onRestoreInstanceState")
     }
